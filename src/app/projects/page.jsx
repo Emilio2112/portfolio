466ef0e988fb9da90d79a731e90bb56a41feaef9
Emilio <emilio_casado_de_galdo@hotmail.com>
2023-09-06 11:50:55 +0100

    Crorreccion de enlaces v3

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,7 +2,7 @@ import Proyecto from "../../../components/Proyecto"
 import styles from "../../../styles/Proyectos.module.css"
 
 export async function getProjects() {
-  const response = await fetch(`${process.env.API_URL}/proyectos?populate=*`)
+  const response = await fetch(`${process.env.API_URL}/proyectos`)
   const respuesta = await response.json()
   return respuesta.data
 }
