65d34d2e660c4e0a5559db1978a6780069ca7f40
Emilio <emilio_casado_de_galdo@hotmail.com>
2026-06-03 22:59:02 +0100

    feat: simplificar la estructura del componente Home eliminando código innecesario

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,22 @@
-export const dynamic = 'force-dynamic';
-import Image from 'next/image'
 import styles from '../../styles/Inicio.module.css'
 import Carousel from '../../components/Carousel'
 
 export default async function Home() {
-
   return (
     <div>
-    <div className={styles.intro}>
-      <div className={styles.presentacion}>
-        <label>
-          Full Stack Developer
-        </label>
-        <ul>
-          <li>Front-End</li>
-          <li>Back-End</li>
-          <li>Ux-Ui</li>
-        </ul>
-        <div className={styles.tecnologias}>
+      <div className={styles.intro}>
+        <div className={styles.presentacion}>
+          <label>Full Stack Developer</label>
+          <ul>
+            <li>Front-End</li>
+            <li>Back-End</li>
+            <li>Ux-Ui</li>
+          </ul>
+          <div className={styles.tecnologias}></div>
         </div>
+        <img className={styles.image} width={500} height={500} src="/images/avatar.jpg" alt='avatar' />
       </div>
-      <img className={styles.image} width={500} height={500} src="/images/avatar.jpg" alt='avatar'  ></img>
-    </div>
-    <Carousel />
+      <Carousel />
     </div>
   )
 }
