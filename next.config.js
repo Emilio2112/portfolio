741a40a9a07ad23d0e504ba31c964571965cd7bf
Emilio <emilio_casado_de_galdo@hotmail.com>
2026-06-03 21:27:03 +0100

    env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com']
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      // Si tu API_URL también es externa (ej. Railway, Render, Heroku), añádela aquí también:
+      // {
+      //   protocol: 'https',
+      //   hostname: 'tu-api-en-produccion.com',
+      //   pathname: '/**',
+      // },
+    ],
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
